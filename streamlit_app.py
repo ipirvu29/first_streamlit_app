dad2a6f0d83dc04626b1fe4023fc7b5617aca5d1
Ioana Pirvu <122103371+ipirvu29@users.noreply.github.com>
2023-01-11 17:41:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error import URLError
+from urllib.error import URLError
 
 #import streamlit
 streamlit.title('My parents New Healthy Dinner')
